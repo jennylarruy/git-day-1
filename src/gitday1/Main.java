cb6edcde5fd1fb75c6c4da81bf1dbb64c60b6cb4
Jenny Larruy <jenny.larruy@gmail.com>
2021-06-07 11:48:26 +0200

    increment

---
+++ src/gitday1/Main.java
@@ -0,0 +1,15 @@
+package gitday1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(increment(2));
+
+    }
+
+    private static int increment (int i){
+        i++;
+        return i;
+    }
+
+}
